d75bb4e654097763ee29fca0b7f3da6101529a74
Jite01 <jamesoyibode968@gmail.com>
2024-10-29 13:49:02 +0100

    assn extra

--- JavaAssignment/SumLoop1.java
+++ JavaAssignment/SumLoop1.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 public class SumLoop1 {
 public static void main (String[] args) {
 Scanner scanner = new Scanner(System.in);
-int nextAction2 = 0;
+int nextAction = 0;
 do {
 System.out.println("Enter 2 numbers chico:");
-int valueC = scanner.nextInt();
-int valueD = scanner.nextInt();
-int result2 = valueC + valueD;
-System.out.printf("Sum is %d, chico%n", result2);
+int valueA = scanner.nextInt();
+int valueB = scanner.nextInt();
+int result = valueA + valueB;
+System.out.printf("Sum is %d, chico%n", result);
 System.out.printf("Another time chico?%n%nPress 0 to quit or 1 to continue:%n");
-nextAction2 = scanner.nextInt();
+nextAction = scanner.nextInt();
 }
-while (nextAction2 == 1);
-if (nextAction2 != 0 && nextAction2 != 1 ) {
+while (nextAction == 1);
+if (nextAction != 0 && nextAction != 1 ) {
 System.out.print("Invalid input. Program has ended");
 return; 
  }
